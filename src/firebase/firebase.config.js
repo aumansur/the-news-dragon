6a5b3aab9448fcd20ddff439a0df746e0e28bdae
aumansur <ahammadullah2001@gmail.com>
2023-04-25 21:54:13 +0600

    the news dragon first days

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAV0UfSrekOId2Wwi8M-Wtmj-85SQF5RDU",
+  authDomain: "the-news-dragon-dc909.firebaseapp.com",
+  projectId: "the-news-dragon-dc909",
+  storageBucket: "the-news-dragon-dc909.appspot.com",
+  messagingSenderId: "522116070622",
+  appId: "1:522116070622:web:7b546a91a190625588d64f",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
---
+++ src/pages/Shared/RightNav/RightNav.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Button, ListGroup } from 'react-bootstrap'; 
+import { BsGoogle, BsGithub, BsFacebook, BsTwitter, BsInstagram } from 'react-icons/bs';
+import QZone from '../QZone/QZone';
+
+const RightNav = () => {
+    return (
+        <div>
+            <h4> Login With</h4>
+            <Button className='mb-2' variant="outline-primary"><BsGoogle></BsGoogle> Login with google </Button>
+            <Button variant="outline-secondary"><BsGithub /> Log in with Github</Button>
+            <div>
+                <h4>Find us on </h4>
+                <ListGroup>
+                    <ListGroup.Item><BsFacebook /> Facebook </ListGroup.Item>
+                    <ListGroup.Item><BsTwitter /> Twitter </ListGroup.Item>
+                    <ListGroup.Item><BsInstagram /> Instagram </ListGroup.Item>
+
+                </ListGroup>
+
+            </div>
+            <QZone></QZone>
+        </div>
+    );
+};
+
+export default RightNav;
